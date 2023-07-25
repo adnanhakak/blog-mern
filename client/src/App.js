aaed791e87cc3274fb1186fe03a51148ca907ce2
INAZBLLP000562\user <inindc00167@azentio.com>
2023-07-25 14:37:40 +0530

    new toast component

--- client/src/App.js
+++ client/src/App.js
@@ -8,31 +8,23 @@ import { UserContextProvider } from "./UserContext";
 import CreatePost from "./pages/CreatePost";
 import PostPage from "./pages/PostPage";
 import EditPost from "./pages/EditPost";
-import { ToastContainer } from 'react-toastify';
+import { EnableToast } from './EnableToast';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <UserContextProvider>
-      <ToastContainer
-        position="top-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        theme="light"
-      />
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<IndexPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/create" element={<CreatePost />} />
-            <Route path="/post/:id" element={<PostPage />} />
-            <Route path="/edit/:id" element={<EditPost />} />
-          </Route>
-        </Routes>
+      <EnableToast />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/edit/:id" element={<EditPost />} />
+        </Route>
+      </Routes>
     </UserContextProvider >
   );
 }
---
+++ client/src/EnableToast.js
@@ -0,0 +1,14 @@
+import { ToastContainer } from 'react-toastify';
+
+export const EnableToast = () => {
+    return <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        theme="light"
+    />
+}
+
